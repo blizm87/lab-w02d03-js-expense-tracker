1456e80753264bd7e1aac610a346739fea6d7bd4
Justin Samuels <blizm87@gmail.com>
2016-12-08 12:39:21 -0800

    Expense tracker takes and posts input, adds and renders total value of expenses.

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ var tbody = document.querySelector('tbody');
 var btn = document.querySelector('button');
 var expName = document.querySelector('input[name="exp-name"]');
 var expAmount = document.querySelector('input[name="exp-amount"]');
+//we can name variables with dollar signs
+//to communicate that its a DOM node
+var $total = document.querySelector('.exp-total');
+var expTotal = document.querySelector('.exp-total');
+var total = 0;
 
 var handleClick = function(evt){
   //prevents form from making an http request.
   evt.preventDefault();
   var expense = getValues();
+  if (expense.name == '' || expense.amount == NaN){
+    return false;
+  }
+  updateTotal(expense.amount);
   var row = renderRow(expense.name,expense.amount);
   tbody.appendChild(row);
   clearInputs();
+}
+
+var updateTotal = function(amount) {
+  total += amount;
+  expTotal.textContent = total;
 }
 
 btn.addEventListener('click', handleClick);
@@ -31,11 +45,11 @@ var renderRow = function(name, amount) {
 }
 
 var getValues = function() {
-  var val = parseFloat(expAmount.value);
+  var val = parseFloat(expAmount.value) || 0;
 
   return {
     name: expName.value,
-    value: val
+    amount: val
   }
 }
 
